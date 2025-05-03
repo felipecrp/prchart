7a44f049e1286262097380392524cf45da1f9034
Felipe Curty <felipecrp@gmail.com>
2025-05-03 11:19:45 -0300

    Add initial docs

--- src/prchart/__init__.py
+++ src/prchart/__init__.py
@@ -1,4 +1,26 @@
+"""
+Publish Ready Chart
 
+
+import prchart as pr
+import prchart.bind as bind
+import prchart.chart as chart
+
+
+```
+(pr
+    .data(
+        dataframe,
+        bind.Category(),
+        bind.Value()
+    )
+    .plot(
+        chart.Bar()
+    )
+)
+```
+
+"""
 import pandas as pd
 
 import prchart.plot
